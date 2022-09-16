411da0b23f7d7dc7d920585db8eba5356a7c9e67
Jeronimo Garcia <garciaj.uk@gmail.com>
2022-09-16 14:20:50 +0100

    being dumb

--- vars/jerry.groovy
+++ vars/jerry.groovy
@@ -2,11 +2,11 @@ def hi(message,url){
     echo "comming from lib ${message}"
     try {
         def res = httpRequest(
-            url: "${url}:80",
+            url: "${url}",
             timeout: 2,
         )
     } catch (Exception ex){
-        println("There's been some error but we are carrying on:"+getMessage())
+        println("There's been some error but we are carrying on")
     }
     finally {
         println(res.status)
