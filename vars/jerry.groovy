4117428733fcce12c9e38a004b512096c6bd0480
Jeronimo Garcia <garciaj.uk@gmail.com>
2022-09-16 12:15:02 +0100

    exception

--- vars/jerry.groovy
+++ vars/jerry.groovy
@@ -1,7 +1,11 @@
 def hi(message){
 echo "comming from lib ${message}"
+    try {
     httpRequest(
         url: "https://www.com.ar",
     )
+    } catch (Exception ex){
+        println(ex)
+    }
 
 }
