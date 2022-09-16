336c6f546b8ee2c1a4643b97f432516657ca6878
Jeronimo Garcia <garciaj.uk@gmail.com>
2022-09-16 13:13:27 +0100

    i

--- vars/jerry.groovy
+++ vars/jerry.groovy
@@ -3,6 +3,7 @@ def hi(message,url){
     try {
         httpRequest(
             url: "${url}:2222",
+            timeout: 2,
         )
     } catch (Exception ex){
         println(ex.message.get.Message())
